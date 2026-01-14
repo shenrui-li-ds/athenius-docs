664c509b55b5fa073901594c29c0f069e88d5d90
Shenrui Li <raylee945@foxmail.com>
2026-01-13 20:32:40 -0500

    Add inline SVG

--- src/lib/branding.ts
+++ src/lib/branding.ts
@@ -1,2 +1,2 @@
-export const APP_ICON = 'https://mmejhlyrwrqsvpmhasvg.supabase.co/storage/v1/object/public/branding/noun-owl-ok.svg';
+export const APP_ICON = 'https://mmejhlyrwrqsvpmhasvg.supabase.co/storage/v1/object/public/branding/853839-temp.svg';
 export const APP_NAME = 'Athenius Docs';
